d4f0cceb732e4e08798eecdf37918ce202ed3a1e
Neel Smith <dnsmith.neel@gmail.com>
2014-04-09 16:54:59 -0400

    Test reduceToWork method.

--- src/test/groovy/edu/harvard/chs/cite/TestCtsUrnUtils.groovy
+++ src/test/groovy/edu/harvard/chs/cite/TestCtsUrnUtils.groovy
@@ -24,17 +24,23 @@ class TestCtsUrnUtils extends GroovyTestCase {
 
 
   void testReduceToWork() {
+    String groupString = "urn:cts:greekLit:tlg0012"
+    CtsUrn groupUrn = new CtsUrn(groupString)
+    assert shouldFail {
+      String reduction = groupUrn.reduceToWork()
+    }
+
     String versString = "urn:cts:greekLit:tlg0012.tlg001.msA:1.1"
     CtsUrn versUrn = new CtsUrn(versString)
     assert versUrn.reduceToWork() == "urn:cts:greekLit:tlg0012.tlg001:1.1"
 
-
     String workString = "urn:cts:greekLit:tlg0012.tlg001:1.1"
     CtsUrn workUrn = new CtsUrn(workString)
-    String groupString = "urn:cts:greekLit:tlg0012"
-    CtsUrn groupUrn = new CtsUrn(groupString)
+    assert workUrn.reduceToWork() == "urn:cts:greekLit:tlg0012.tlg001:1.1"
+
     String emptyPsgString =  "urn:cts:greekLit:tlg0012.tlg001.msA:"
     CtsUrn emptyPsgUrn = new CtsUrn(emptyPsgString)
+    assert emptyPsgUrn.reduceToWork() == "urn:cts:greekLit:tlg0012.tlg001:"
 
   }
 
