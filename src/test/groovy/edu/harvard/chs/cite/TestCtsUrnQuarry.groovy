dcf80b72afd31e2e272f86242105a2411d039d73
Neel Smith <dnsmith.neel@gmail.com>
2014-12-19 13:13:39 -0500

    Trailing : required

--- src/test/groovy/edu/harvard/chs/cite/TestCtsUrnQuarry.groovy
+++ src/test/groovy/edu/harvard/chs/cite/TestCtsUrnQuarry.groovy
@@ -10,67 +10,67 @@ import org.junit.Test
 class TestCtsUrnQuarry extends GroovyTestCase {
 
 
-    def subrefEg = "urn:cts:latinLit:stoa0115.stoa002:preface.1@nunc"
+  def subrefEg = "urn:cts:latinLit:stoa0115.stoa002:preface.1@nunc"
 
-    /**
-    * Tests basic methods of CtsUrn class.
-    */
-    void testPointUrn() {
-	 // only form of constructor: URN from String:
-	 shouldFail {
-	 	    def badUrnSyntax = new CtsUrn("Dumb string")
-	 }
-	 shouldFail {
-	 	    def tooShortSyntax = new CtsUrn("urn:cts:greekLit:")
-	 }
+  /**
+   * Tests basic methods of CtsUrn class.
+   */
+  void testPointUrn() {
+    // only form of constructor: URN from String:
+    shouldFail {
+      def badUrnSyntax = new CtsUrn("Dumb string")
+    }
+    shouldFail {
+      def tooShortSyntax = new CtsUrn("urn:cts:greekLit:")
+    }
 
-      	 def testUrnStr = "urn:cts:greekLit:tlg1220.tlg001:1" 
-	 CtsUrn urn = new CtsUrn(testUrnStr)
+    def testUrnStr = "urn:cts:greekLit:tlg1220.tlg001:1" 
+    CtsUrn urn = new CtsUrn(testUrnStr)
 
-	 // round trips OK:
-	 assert urn.toString() == testUrnStr
+    // round trips OK:
+    assert urn.toString() == testUrnStr
 
-	 // access top-level components:
-	 assert urn.getCtsNamespace() == "greekLit"
-	 assert urn.getWorkComponent() == "tlg1220.tlg001"
-	 assert urn.getPassageComponent() == '1'
+    // access top-level components:
+    assert urn.getCtsNamespace() == "greekLit"
+    assert urn.getWorkComponent() == "tlg1220.tlg001"
+    assert urn.getPassageComponent() == '1'
 
-	 // access portions of reference component:
-	 assert urn.getLeafRefValue() == "1"
-
-
-	 assert urn.getTextGroup(true) == "greekLit:tlg1220"
-	 assert urn.getWork(true) == "greekLit:tlg001"
-
-	 assert urn.getTextGroup(false) == "tlg1220"
-	 assert urn.getWork(false) == "tlg001"
+    // access portions of reference component:
+    assert urn.getLeafRefValue() == "1"
 
 
-	 assert urn.getTextGroup() == "tlg1220"
-	 assert urn.getWork() == "tlg001"
+    assert urn.getTextGroup(true) == "greekLit:tlg1220"
+    assert urn.getWork(true) == "greekLit:tlg001"
+    
+    assert urn.getTextGroup(false) == "tlg1220"
+    assert urn.getWork(false) == "tlg001"
 
 
-	 // A full URN for the work component is
-	 // useful for many applications:
-	 assert urn.getUrnWithoutPassage() == "urn:cts:greekLit:tlg1220.tlg001"
-
-	 // level within work hiearchy is an enum that can be converted
-	 // to and from String values:
-	 assert urn.labelForWorkLevel() == "work"
-	 assert urn.levelForLabel(urn.labelForWorkLevel()) == CtsUrn.WorkLevel.WORK	
+    assert urn.getTextGroup() == "tlg1220"
+    assert urn.getWork() == "tlg001"
 
 
-         urn = new CtsUrn("urn:cts:greekLit:tlg1220.tlg001.chs01")
-         assert urn.getVersion(true) == "greekLit:chs01"
-         assert urn.getVersion(false) == "chs01"
-         assert urn.getVersion() == "chs01"
+    // A full URN for the work component is
+    // useful for many applications:
+    assert urn.getUrnWithoutPassage() == "urn:cts:greekLit:tlg1220.tlg001:"
+    
+    // level within work hiearchy is an enum that can be converted
+    // to and from String values:
+    assert urn.labelForWorkLevel() == "work"
+    assert urn.levelForLabel(urn.labelForWorkLevel()) == CtsUrn.WorkLevel.WORK	
 
-         urn = new CtsUrn("urn:cts:latinLit:stoa0054.stoa010.hc")
-	 println urn.getVersion()
 
-
+    urn = new CtsUrn("urn:cts:greekLit:tlg1220.tlg001.chs01")
+    assert urn.getVersion(true) == "greekLit:chs01"
+    assert urn.getVersion(false) == "chs01"
+    assert urn.getVersion() == "chs01"
+    
+    urn = new CtsUrn("urn:cts:latinLit:stoa0054.stoa010.hc")
+    println urn.getVersion()
  
-     }
+  }
+
+  
 
      /**
      * Tests methods specific to range Urns.
