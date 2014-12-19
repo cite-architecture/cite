bb2f6e864c8daea80e39248b343b05555f084b8b
Neel Smith <dnsmith.neel@gmail.com>
2014-12-19 13:18:57 -0500

    Add check on Form.NFC

--- src/test/groovy/edu/harvard/chs/cite/TestCtsUrnToString.groovy
+++ src/test/groovy/edu/harvard/chs/cite/TestCtsUrnToString.groovy
@@ -4,27 +4,26 @@ package edu.harvard.chs.cite
 import static org.junit.Assert.*
 import org.junit.Test
 
+import java.text.Normalizer
+import java.text.Normalizer.Form
+
+
 class TestCtsUrnToString extends GroovyTestCase {
 
-     void testPointUrn() {
-       String  pointUrnStr = "urn:cts:greekLit:tlg0012.tlg001:1.1@μῆνιν[1]" 
-       CtsUrn pointUrn = new CtsUrn(pointUrnStr)
-       assert pointUrn.toString() == pointUrnStr
+  //  Form.NFC valid in RDF, so toString() method of CtsUrn class
+  // alwqys returns Form.NFC.
+  void testPointUrn() {
+    String  pointUrnStr = "urn:cts:greekLit:tlg0012.tlg001:1.1@μῆνιν[1]" 
+    CtsUrn pointUrn = new CtsUrn(pointUrnStr)
+    assert pointUrn.toString() == Normalizer.normalize(pointUrnStr, Form.NFC)
+  }
 
-       String encodedStr = pointUrn.toString(true)
-       System.err.println encodedStr
-       assert pointUrnStr == java.net.URLDecoder.decode(encodedStr, "UTF-8")
-     }
-
-     void testRangeUrn() {
-      	 String rangeUrnStr = "urn:cts:greekLit:tlg0012.tlg001:1.1-1.2@ν[2]" 
-	 CtsUrn rangeUrn = new CtsUrn(rangeUrnStr)
-	 assert rangeUrn.isRange()
-
-	 String encodedStr = rangeUrn.toString(true)
-       	 System.err.println encodedStr		
-	 assert java.net.URLDecoder.decode(encodedStr,"UTF-8") == rangeUrnStr
-    }
+  void testRangeUrn() {
+    String rangeUrnStr = "urn:cts:greekLit:tlg0012.tlg001:1.1-1.2@ν[2]" 
+    CtsUrn rangeUrn = new CtsUrn(rangeUrnStr)
+    assert rangeUrn.isRange()
+    assert rangeUrn.toString() == Normalizer.normalize(rangeUrnStr, Form.NFC)
+  }
 
 
  
