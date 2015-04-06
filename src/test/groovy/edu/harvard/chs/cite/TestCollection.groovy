524ab21a02ba5ac9785806a5c2c0a4edbc25569b
Neel Smith <dnsmith.neel@gmail.com>
2015-04-06 13:35:21 -0400

    Add placeholder for unit test

---
+++ src/test/groovy/edu/harvard/chs/cite/TestCollection.groovy
@@ -0,0 +1,20 @@
+package edu.harvard.chs.cite 
+
+
+import static org.junit.Assert.*
+import org.junit.Test
+
+
+/** Class to test cite library's CiteCollection class. 
+*/
+class TestCollection extends GroovyTestCase {
+
+  
+  void testOneCollection() {
+    CiteCollection cc = new CiteCollection()
+    
+  }
+
+  
+ 
+}
