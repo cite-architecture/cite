2ae48e6a00701481320f9348683ed0115be1dc63
Neel Smith <dnsmith.neel@gmail.com>
2015-12-01 10:17:15 -0500

    Upgrade to junit 1.4 and conc 1.5.12

--- src/test/groovy/edu/harvard/chs/cite/TestCtsUrnManip.groovy
+++ src/test/groovy/edu/harvard/chs/cite/TestCtsUrnManip.groovy
@@ -1,34 +1,36 @@
-package edu.harvard.chs.cite 
+package edu.harvard.chs.cite
 
-
-import static org.junit.Assert.*
 import org.junit.Test
+import static groovy.test.GroovyAssert.shouldFail
 
 
+/** Tests demonstrating parsing of nouns from Unicode string.
+*/
+class TestCtsUrnManip {
 
-class TestCtsUrnManip extends GroovyTestCase {
-  
 
+  @Test
   void testNodeManipulation() {
     CtsUrn versionRef = new CtsUrn("urn:cts:greekLit:tlg0012.tlg001.msA:1.1@μῆνιν")
     String actual = versionRef.reduceToNode()
     String expected = "urn:cts:greekLit:tlg0012.tlg001.msA:1.1"
     assert actual == expected
-    
+
     CtsUrn workRef = new CtsUrn("urn:cts:greekLit:tlg0012.tlg001:1.1@μῆνιν")
     assert workRef.reduceToNode() == "urn:cts:greekLit:tlg0012.tlg001:1.1"
 
   }
 
+  @Test
   void testRangeManipulation() {
 
     CtsUrn versionRange = new CtsUrn("urn:cts:greekLit:tlg0012.tlg001.msA:1.1-1.2@οὐλομένην")
     assert versionRange.reduceToNode() == "urn:cts:greekLit:tlg0012.tlg001.msA:1.1-1.2"
 
-    
+
     CtsUrn workRange = new CtsUrn("urn:cts:greekLit:tlg0012.tlg001.msA:1.1-1.2@οὐλομένην")
     assert workRange.reduceToNode() == "urn:cts:greekLit:tlg0012.tlg001.msA:1.1-1.2"
-    
+
   }
-  
+
 }
