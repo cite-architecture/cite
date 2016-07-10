7a657ea44575660eaa7f14459c22b2d6d11906f0
Neel Smith <dnsmith.neel@gmail.com>
2016-07-10 15:14:36 +0200

    Complete conversion of all tests formerly in big single file.  All now comply with syntax requirements for extended references.

--- src/test/groovy/edu/harvard/chs/cite/TestCiteUrnConstructBadRangeExtensions.groovy
+++ src/test/groovy/edu/harvard/chs/cite/TestCiteUrnConstructBadRangeExtensions.groovy
@@ -8,7 +8,7 @@ class TestCiteUrnConstructBadRangeExtensions {
 
   // Range Object, noVersion-version, extendedRef-noExtendedRef
   @Test
-  void testConstructor18() {
+  void testConstructorError() {
     assert shouldFail {
       CiteUrn versObj = new CiteUrn("urn:cite:hmt:vaimg.VA012RN_0013@0.1,0.1,0.1,0.1-VA024RN_0025.v1")
     }
@@ -17,7 +17,7 @@ class TestCiteUrnConstructBadRangeExtensions {
   // But this is OK:
   // Range Object, version-Noversions, extendedRef-noExtendedRef
   @Test
-  void testConstructor() {
+  void testConstructor1() {
    
    CiteUrn versObj = new CiteUrn("urn:cite:hmt:vaimg.VA012RN_0013.v1@0.1,0.1,0.1,0.1-VA024RN_0025")
    assert versObj.asString == "urn:cite:hmt:vaimg.VA012RN_0013.v1@0.1,0.1,0.1,0.1-VA024RN_0025.v1"
@@ -35,5 +35,24 @@ class TestCiteUrnConstructBadRangeExtensions {
    assert versObj.objectVersion_2 == "v1"
   }
 
+
+  // as is this:
+  @Test
+  void testConstructor2() {
+   CiteUrn versObj = new CiteUrn("urn:cite:hmt:vaimg.VA012RN_0013-VA024RN_0025.v1@0.2,0.2,0.2,0.2")
+   assert versObj.asString == "urn:cite:hmt:vaimg.VA012RN_0013.v1-VA024RN_0025.v1@0.2,0.2,0.2,0.2"
+   assert versObj.collection == "vaimg"
+   assert versObj.extendedRef == null
+   assert versObj.extendedRef_1 == null
+   assert versObj.extendedRef_2 == "0.2,0.2,0.2,0.2"
+   assert versObj.ns == "hmt"
+   assert versObj.objectComponent == "vaimg.VA012RN_0013.v1-VA024RN_0025.v1@0.2,0.2,0.2,0.2"
+   assert versObj.objectId == null
+   assert versObj.objectId_1 == "VA012RN_0013"
+   assert versObj.objectId_2 == "VA024RN_0025"
+   assert versObj.objectVersion == null
+   assert versObj.objectVersion_1 == "v1"
+   assert versObj.objectVersion_2 == "v1"
+  }
 
 }
